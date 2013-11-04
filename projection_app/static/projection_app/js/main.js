d6a74b85e80dc02471b1f726bacf9017012428e3
bwreilly <benwreilly@gmail.com>
2013-11-03 16:24:06 -0800

    update projection data dynamically

--- projection_app/static/projection_app/js/main.js
+++ projection_app/static/projection_app/js/main.js
@@ -20,21 +20,38 @@ MainView = Backbone.View.extend({
     view.map.render();
   },
   update: function(item) {
-    this.map.name = item;
-    this.map.collection.fetch({data: {"name": item}, 'reset': true});
+    var view = this;
+    $.getJSON('api/occupations/', {'name': item, 'format': 'json'},
+      function(d) {
+        view.chart.data({"projections": d.results}).update();
+    });
+    
+    // this.map.name = item;
+    // this.map.collection.fetch({data: {"name": item}, 'reset': true});
     return item;
   },
   initialize: function() {
     _.bindAll(this);
     var view = this;
-    view.map = new StateMap();
+    // view.map = new StateMap();
     $('#search').typeahead({
       updater: view.update,
       source: view.names
     });
+    view.render();
+  },
+  render: function() {
+    var view = this;
+    function parse(spec) {
+      vg.parse.spec(spec, function(chart) {
+        view.chart = chart({el:"#mapcontainer", renderer:"svg"}).update();
+      });
+    }
+    parse("static/projection_app/js/map-spec.json");
   }
 });
 
 $(function() {
   var main = new MainView();
+  console.log("welp");
 });
